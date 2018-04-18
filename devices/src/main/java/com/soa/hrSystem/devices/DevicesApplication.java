940c3043ff9bbd35012df4d8840acefff4975d2f
Dario Markovski <dario.markovski@outlook.com>
2018-04-18 19:11:09 +0200

    commit

--- devices/src/main/java/com/soa/hrSystem/devices/DevicesApplication.java
+++ devices/src/main/java/com/soa/hrSystem/devices/DevicesApplication.java
@@ -2,7 +2,9 @@ package com.soa.hrSystem.devices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DevicesApplication {
 
